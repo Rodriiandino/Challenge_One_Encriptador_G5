2cdcf8050b57894903e1f3e031723901381640bd
RodriiAndino <andinorodrigo03@gmail.com>
2023-04-17 12:25:22 -0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: 'https://Rodriiandino.gitlab.io/encriptador-oracle-alura'
+  base: 'https://Rodriiandino.github.io/Challenge_One_Encriptador_G5'
 })
